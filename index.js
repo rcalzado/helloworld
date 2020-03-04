e1bdd92c2fe1312e3dfa3f333b32553b04fb5d4e
rcalzado <32982341+rcalzado@users.noreply.github.com>
2020-03-03 21:53:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/',  (req, res) => res.send('R$ 750,00 na minha conta já!'));
+app.get('/',  (req, res) => res.send('Running!'));
 
 app.listen(3000);
