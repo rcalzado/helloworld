bed4a548954a65c2d8e598fe7a3d9b76c6fbb11e
rcalzado <32982341+rcalzado@users.noreply.github.com>
2020-03-17 23:53:13 -0300

    update build

--- build/build.sh
+++ build/build.sh
@@ -2,7 +2,7 @@
 REGISTRY=localhost:5000
 
 docker images $REGISTRY/$1 -q | xargs --no-run-if-empty docker rmi -f
-docker build -t $REGISTRY/$1:$2 . 
+docker build -t $REGISTRY/$1:$2 -f ../Dockerfile
 docker tag $REGISTRY/$1:$2 $REGISTRY/$1:latest
 docker push $REGISTRY/$1:latest
 docker rmi $REGISTRY/$1:$2
